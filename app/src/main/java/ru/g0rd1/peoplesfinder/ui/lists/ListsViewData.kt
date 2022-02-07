1da184334634cf77bc22fe57f9a681ec68b7d109
g0rd1 <gordigev@gmail.com>
2022-02-08 00:11:49 +0500

    add immutable annotation

--- app/src/main/java/ru/g0rd1/peoplesfinder/ui/lists/ListsViewData.kt
+++ app/src/main/java/ru/g0rd1/peoplesfinder/ui/lists/ListsViewData.kt
@@ -1,10 +1,15 @@
 package ru.g0rd1.peoplesfinder.ui.lists
 
+import androidx.compose.runtime.Immutable
 import ru.g0rd1.peoplesfinder.model.User
 import ru.g0rd1.peoplesfinder.model.UserType
 
+@Immutable
 sealed class ListsViewData {
+    @Immutable
     object Loading : ListsViewData()
+
+    @Immutable
     data class Data(
         val userType: UserType,
         val users: List<User>,
